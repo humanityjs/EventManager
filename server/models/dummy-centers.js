93a8b5f0b763b2e5e3ce9b82276c2a3505dc5d3f
Orlayhemmy <daminomics@gmail.com>
2017-11-28 16:10:38 -1100

    FEATURES# add models and migration files

---
+++ server/models/dummy-centers.js
@@ -0,0 +1,41 @@
+const centers = [
+    {
+        id:1,
+        name:"Balmoral",
+        location: "Ojota",
+        facilities: "Projector, Stage lights",
+        description: "A world class event center with ample car park space",
+    },
+    {
+        id:2,
+        name:"Faith Plaza",
+        location: "Bariga",
+        facilities: "Stage lights, Large Dinning, Guest house",
+        description: "An event center for large occasions coupled with lodging space",
+    },
+    {
+        id:3,
+        name:"Ara Hall",
+        location: "Ibafo",
+        facilities: "Quality stage sound, Projector",
+        description: "An event center suited to your budget",
+    },
+    {
+        id:4,
+        name:"Five Points",
+        location: "Lekki",
+        facilities: "Guest house, Dinning Area, Stage and Lights",
+        description: "A world class event center",
+    },
+    {
+        id:5,
+        name:"Radisson Blu",
+        location: "Ozumba",
+        facilities: "Dinning Area, Stage and Lights",
+        description: "An event center with style",
+    }
+    
+];
+
+export default centers;
+
