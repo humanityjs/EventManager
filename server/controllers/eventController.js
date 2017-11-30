6b39ccb81caaf9fd711750e01cfb4640686c6d3f
Orlayhemmy <daminomics@gmail.com>
2017-11-29 20:29:22 -1100

    FEATURES# make changes to eventcontroller

--- server/controllers/eventController.js
+++ server/controllers/eventController.js
@@ -45,7 +45,52 @@ class EventController {
     // unauthorised user
     return res.status(403).json({
       message: 'You are not authorised to view this page',
-      isAdmin,
+    });
+  }
+
+  static updateEvent(req, res) {
+    const {
+      title, description, bookedDate, centerId,
+    } = req.body;
+    const { id, isAdmin } = req.decoded;
+
+    if (!isAdmin) {
+      // query db
+      return Events.findOne({
+        where: {
+          centerId, bookedDate,
+        },
+      }).then((event) => {
+        if (event) {
+          return res.status(400).json({
+            message: 'Center is not available for the date chosen',
+          });
+        }
+        return Events.update({
+          title: title || event.title,
+          bookedDate: bookedDate || event.bookedDate,
+          description: description || event.description,
+          centerId: centerId || event.centerId,
+          userId: id,
+        }, {
+          where: {
+            id: req.params.id,
+          },
+        }).then((modifiedEvent) => {
+          res.status(200).json({
+            message: 'Changes Applied',
+            modifiedEvent,
+          });
+        }).catch(error => res.status(500).json({
+          message: error.message,
+        }));
+      }).catch(error => res.status(500).json({
+        message: error.message,
+      }));
+    }
+    // unauthorised user
+    return res.status(403).json({
+      message: 'You are not authorised to view this page',
     });
   }
 }
