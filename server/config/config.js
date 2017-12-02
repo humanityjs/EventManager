2d902c6a4660fa1735ab0a4d0a8846b85c9b1f39
Orlayhemmy <daminomics@gmail.com>
2017-12-02 06:30:15 -1100

    FIXES# add command for travis to install db

--- server/config/config.js
+++ server/config/config.js
@@ -12,7 +12,7 @@ module.exports = {
   test: {
     username: 'postgres',
     password: 'profyem001',
-    database: 'eventManager',
+    database: 'travis_db',
     host: '127.0.0.1',
     dialect: 'postgres',
     port: 5432,
