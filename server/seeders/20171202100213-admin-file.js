fbfc9625af6f76e227124b44b0ae3b03b1e5f2cf
Orlayhemmy <daminomics@gmail.com>
2017-12-02 03:56:36 -1100

    FIXES# correction in apiTest file

--- server/seeders/20171202100213-admin-file.js
+++ server/seeders/20171202100213-admin-file.js
@@ -2,7 +2,7 @@ module.exports = {
   up: (queryInterface, Sequelize) => queryInterface.bulkInsert('Users', [{
     fullname: 'John',
     password: '$2a$10$/HaQApiK8ZEuZcmgWAUHr.fwNMK3iJLMOnSWK3yGeor/2cuubnVUC',
-    email: 'john@test.com',
+    email: 'admin@test.com',
     isAdmin: 'true',
     createdAt: new Date(),
     updatedAt: new Date(),
