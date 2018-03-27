3c2ec4830468037a749c125ebfabca3e35b54190
Orlayhemmy <daminomics@gmail.com>
2018-03-27 21:18:21 +0100

    redirect user to dashboard after authentication

--- client/components/homePage.jsx
+++ client/components/homePage.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 import Navbar from './navbar.jsx';
 import Content from './Homepage/homeContent';
 import Footer from './footer';
 
+@connect((store) => {
+  return {
+    auth: store.auth,
+  };
+})
+
 export default class Homepage extends React.Component {
   render() {
 
+    if (this.props.auth.isAuth) {
+      return <Redirect to="/dashboard" />;
+    }
     const { pathname } = this.props.location;
     return (
       <div id="homepage">
